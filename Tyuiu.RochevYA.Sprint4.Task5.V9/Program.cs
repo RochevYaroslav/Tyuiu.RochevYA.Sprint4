5e628a8c923baf6a1e36d6a69ae477fed4d400e9
RochevYaroslav <justmisterstone@gmail.com>
2025-09-29 23:06:17 +0500

    Task5

---
+++ Tyuiu.RochevYA.Sprint4.Task5.V9/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Tyuiu.RochevYA.Sprint4.Task5.V9.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int[,] array = new int[5, 5];
+        for (int i = 0; i < 5; i++)
+        {
+            int[] row = Console.ReadLine().Split(',').Select(int.Parse).ToArray();
+            for (int j = 0; j < 5; j++)
+                array[i, j] = row[j];
+        }
+        Console.WriteLine(ds.Calculate(array));
+        Console.WriteLine("Сделал Рочев Ярослав Антонович | ИБКСб-25-1");
+    }
+}
